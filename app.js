3b8e0ea5fc5125d5cebd84f543ae4ac96f25e410
Arin <arinyazilim@gmail.com>
2021-05-05 18:39:10 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index', {
+      page_name: "index"
+  });
 });
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+  });
 
 const port = 3000;
 app.listen(port, () => {
