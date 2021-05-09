921628f1259be2650b38240dd7c40d51fefcd357
Arin <arinyazilim@gmail.com>
2021-05-09 22:55:42 +0300

    Lesson 16

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
+
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -45,8 +47,10 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({_id:req.session.userID})
+  const categories = await Category.find();
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
-    user
+    user,
+    categories
   });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -5,10 +5,7 @@ exports.createCourse = async (req, res) => {
   try {
     const course = await Course.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      course,
-    });
+    res.status(201).redirect('/courses');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -30,7 +27,7 @@ exports.getAllCourses = async (req, res) => {
       filter = {category:category._id}
     }
 
-    const courses = await Course.find(filter);
+    const courses = await Course.find(filter).sort('-createdAt');
     const categories = await Category.find();
 
     res.status(200).render('courses', {
