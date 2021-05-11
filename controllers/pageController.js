1a4efef851daf44c508476d72dd90fdbf0ebd60f
Arin <arinyazilim@gmail.com>
2021-05-11 14:56:07 +0300

    Lesson 21

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,6 @@
+const nodemailer = require("nodemailer");
+
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -21,4 +24,52 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+
+  const outputMessage = `
+  
+  <h1>Mail Details </h1>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+  `
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "arinyazilim@gmail.com", // gmail account
+      pass: "bpwrtssmqdsdjdjw", // gmail password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart EDU Contact Form" <arinyazilim@gmail.com>', // sender address
+    to: "gcekic@gmail.com", // list of receivers
+    subject: "Smart EDU Contact Form New Message ✔", // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect('contact');
+
 };
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -8,6 +8,8 @@ router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
 router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
 router.route('/login').get(redirectMiddleware, pageController.getLoginPage);
+router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.sendEmail);
 
 module.exports = router;
 
