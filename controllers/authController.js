6d325b395c8d96bb9d25b90ea362f5a7397f89e9
Arin <arinyazilim@gmail.com>
2021-05-12 18:19:08 +0300

    Lesson 23

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Course = require('../models/Course');
@@ -9,10 +10,15 @@ exports.createUser = async (req, res) => {
 
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+    console.log(errors);
+    console.log(errors.array()[0].msg);
+
+    for (let i = 0; i <errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+  
+    res.status(400).redirect('/register');
   }
 };
 
@@ -24,10 +30,19 @@ exports.loginUser = async (req, res) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
 
+          if (same) {
             // USER SESSION
             req.session.userID = user._id;
             res.status(200).redirect('/users/dashboard');
+          } else {
+            req.flash("error", "Your password is not correct!");
+            res.status(400).redirect('/login');
+          }
+
         });
+      } else {
+        req.flash("error", "User is not exist!");
+        res.status(400).redirect('/login');
       }
     });
   } catch (error) {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { body } = require('express-validator');
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
+router.route('/signup').post(
+    [
+        body('name').not().isEmpty().withMessage('Please Enter Your Name'),
+
+
+        body('email').isEmail().withMessage('Please Enter Valid Email')
+        .custom((userEmail)=> {
+            return User.findOne({email:userEmail}).then(user => {
+                if (user) {
+                    return Promise.reject('Email is already exists!')
+                }
+            })
+        }),
+
+        body('password').not().isEmpty().withMessage('Please Enter A Password'),
+    ],
+    
+    authController.createUser); // http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); http://localhost:3000/users/dashboard
